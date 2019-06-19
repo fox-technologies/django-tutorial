a6c2921182f3d3417c9e3a1476c6916aa0e04b4c
Callum Osborn <callum.osborn@smartaction.com>
2019-06-19 06:40:02 -0700

    create two models application and customer

--- src/mysite/polls/models.py
+++ src/mysite/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+  name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
+
+class Application(models.Model):
+  name = models.CharField(max_length=200)
+  customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
